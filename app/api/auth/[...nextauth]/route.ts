bb7d059a1571d778bc758acb208a6b9842f9b4d3
DevBasem <engr.basemahmed@gmail.com>
2024-03-14 15:20:03 -0700

    bug fixes for authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "./allAuthOptions";
+import { authOptions } from "@/app/utils/authOptions";
 
 const handler = NextAuth(authOptions);
 
